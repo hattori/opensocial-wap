5eb256999949ae97d306e93b3f7927e7f53d03d6
Tomonori Kusanagi <kusanagi@banana-systems.com>
2011-01-26 10:22:09 +0900

    Removed the patch for form_for helper.

--- lib/opensocial-wap/railtie.rb
+++ lib/opensocial-wap/railtie.rb
@@ -4,7 +4,6 @@ require 'opensocial-wap/routing/url_for'
 require 'opensocial-wap/helpers/base'
 require 'opensocial-wap/helpers/url_helper'
 require 'opensocial-wap/helpers/form_tag_helper'
-require 'opensocial-wap/helpers/form_helper'
 require 'opensocial-wap/action_controller/controller_hook'
 require 'opensocial-wap/action_controller/metal/redirecting'
 
