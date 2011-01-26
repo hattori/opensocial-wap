b113a8388dda82bd6c87f2d78bfaee4bf426f498
Tomonori Kusanagi <kusanagi@banana-systems.com>
2011-01-26 16:29:45 +0900

    Redirect location for each redirect_url_format.

--- lib/opensocial-wap/action_controller/metal/redirecting.rb
+++ lib/opensocial-wap/action_controller/metal/redirecting.rb
@@ -3,12 +3,26 @@ module OpensocialWap
     module Redirecting
       include ::OpensocialWap::Routing::UrlFor
 
+      def redirect_to(options = {}, response_status = {})
+        super
+      end
+
       private
       
       # 
       def _compute_redirect_to_location(options)
         url = super
-        full_url_for(url, self.opensocial_wap_options, params[:opensocial_app_id])
+        osw_options = self.opensocial_wap_options
+        redirect_url_format = osw_options[:redirect_url_format] || :full
+        
+        case redirect_url_format
+        when :plain
+          url
+        when :query
+          query_url_for(url, osw_options[:params])
+        else
+          full_url_for(url, osw_options, params[:opensocial_app_id])
+        end
       end
     end
   end
