ede0cf730984aaccd206597bfa43eb853a474cb1
Tomonori Kusanagi <kusanagi@banana-systems.com>
2011-01-26 12:28:24 +0900

    Redirect location.

--- lib/opensocial-wap/action_controller/metal/redirecting.rb
+++ lib/opensocial-wap/action_controller/metal/redirecting.rb
@@ -1,11 +1,14 @@
 module OpensocialWap
   module ActionController
     module Redirecting
+      include ::OpensocialWap::Routing::UrlFor
 
       private
+      
+      # 
       def _compute_redirect_to_location(options)
-puts '_compute_redirect_to_location'
-        super
+        url = super
+        full_url_for(url, self.opensocial_wap_options, params[:opensocial_app_id])
       end
     end
   end
