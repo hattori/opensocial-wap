76b297a654d49f20b0f15127c595b3dd1df567cc
hattori <hattori@banana-systems.com>
2011-01-28 17:08:32 +0900

    rack middleware stack 内で OpensocialWap::Rack::OpensocialOauth middleware を ActionDispatch::Cookies の前に挿入

--- lib/opensocial-wap/railtie.rb
+++ lib/opensocial-wap/railtie.rb
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+require 'opensocial-wap/rack/opensocial_oauth'
+require 'opensocial-wap/routing/url_for'
 require 'opensocial-wap/routing/url_for'
 require 'opensocial-wap/helpers/base'
 require 'opensocial-wap/helpers/url_helper'
@@ -9,9 +11,24 @@ require 'opensocial-wap/action_controller/metal/redirecting'
 
 module OpensocialWap
   class Railtie < Rails::Railtie
+
+     config.opensocial_wap = ActiveSupport::OrderedOptions.new
+
     initializer "opensocial-wap.initialize" do |app|
       # do something..
-
     end
+
+    initializer 'opensocial-wap.load_middleware', :after=> :load_config_initializers do
+      platform = config.opensocial_wap.platform
+
+      log_level = config.opensocial_wap.log_level || :error
+      if platform
+        puts "opensocial-wap is enabled with #{platform.class}"
+        config.app_middleware.insert_before ActionDispatch::Cookies,  OpensocialWap::Rack::OpensocialOauth, :platform=>platform, :log_level=>log_level.to_s.upcase
+      else
+        puts "opensocial-wap is NOT enabled" 
+      end
+    end
+
   end
 end
