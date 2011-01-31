9f1529f723ec8a79ba3f6cc56fbd0f90ed8e1be4
Tomonori Kusanagi <kusanagi@banana-systems.com>
2011-01-31 12:19:45 +0900

    Fixed the form_tag double http protocol bug when url is given with path.

--- lib/opensocial-wap/helpers/form_tag_helper.rb
+++ lib/opensocial-wap/helpers/form_tag_helper.rb
@@ -15,13 +15,14 @@ module OpensocialWap
       def html_options_for_form(url_for_options, options, *parameters_for_url)
         # call super unless opensocial_wap is not called in the controller.
         super unless default_osw_options
-
+        
         options.stringify_keys.tap do |html_options|
           html_options["enctype"] = "multipart/form-data" if html_options.delete("multipart")
           # ows_options を、options から取り出す.
           osw_options = default_osw_options.merge(options[:osw_options] || {})
           # The following URL is unescaped, this is just a hash of options, and it is the
           # responsability of the caller to escape all the values.
+
           html_options["action"]  = url_for(url_for_options, osw_options, *parameters_for_url)
           html_options["accept-charset"] = "UTF-8"
           html_options["data-remote"] = true if html_options.delete("remote")
--- lib/opensocial-wap/routing/url_for.rb
+++ lib/opensocial-wap/routing/url_for.rb
@@ -13,8 +13,13 @@ module OpensocialWap
         options ||= {}
         url = case options
               when String
-                opts = options_for_full_url(nil, osw_options)
-                base_url(opts) + options
+                # URL文字列がプロトコル部分を含まなければ、プロトコル、ホスト、ポートを付与する.
+                unless options.scan(%r{^\w[\w+.-]*://}).first
+                  opts = options_for_full_url(nil, osw_options)
+                  base_url(opts) + options
+                else
+                  options
+                end
               when Hash
                 opts = options_for_full_url(options, osw_options)
                 url_for(opts)
