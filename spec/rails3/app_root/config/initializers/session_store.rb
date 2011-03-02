efbd563fca982f2d798dda5a85127068e6f70fdd
Tomonori Kusanagi <kusanagi@banana-systems.com>
2011-03-02 17:02:39 +0900

    User active_record_store as session store for the test app.

--- spec/rails3/app_root/config/initializers/session_store.rb
+++ spec/rails3/app_root/config/initializers/session_store.rb
@@ -1,8 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-AppRoot::Application.config.session_store :cookie_store, :key => '_app_root_session'
+# User active_record_store to test opensocial_wap_sid.
+AppRoot::Application.config.session_store :active_record_store
 
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# AppRoot::Application.config.session_store :active_record_store
--- spec/rails3/app_root/db/schema.rb
+++ spec/rails3/app_root/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110121075848) do
+ActiveRecord::Schema.define(:version => 20110302035421) do
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "name"
