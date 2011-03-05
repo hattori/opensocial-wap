b6cd830c672dfaa8f0770b209e7d77803a667dc3
Tomonori Kusanagi <kusanagi@banana-systems.com>
2011-03-06 03:41:52 +0900

    Specs for redirecting to hash path.

--- spec/rails3/app_root/spec/controllers/redirecting_spec.rb
+++ spec/rails3/app_root/spec/controllers/redirecting_spec.rb
@@ -1,72 +1,94 @@
 # -*- coding: utf-8 -*-
 require 'spec_helper'
 
+module TestController
+  def index
+    format = params[:format] || "polymorphic"
+    case format
+    when "hash"
+      redirect_to :controller => "users", :action => "new"
+    else
+      redirect_to User.new
+    end
+  end
+end
+
 describe OpensocialWap::ActionController::Redirecting do
-  
+
   context "コントローラで opensocial_wap が宣言されていない場合" do
     controller do
-      def index
-        redirect_to User.new
-      end
+      include TestController
     end
 
-    it do
+    it "アプリケーションサーバのホスト名を含めたURLにリダイレクトされること" do
       get :index, :opensocial_app_id => '12345'
       response.should redirect_to("http://test.host/users")
+    end
+    it "アプリケーションサーバのホスト名を含めたURLにリダイレクトされること" do
+      get :index, :format => "hash", :opensocial_app_id => '12345'
+      response.should redirect_to("http://test.host/users/new")
     end
   end
 
   context "コントローラで :redirect_url_format が指定されていない場合" do
     controller do
       opensocial_wap
-      def index
-        redirect_to User.new
-      end
+      include TestController
     end
 
     it "初期化時に指定した :redirect_url_format に従ってリダイレクト先が決定すること" do
       get :index, :opensocial_app_id => '12345'
       response.should redirect_to("http://container.example.com/12345/?guid=ON&url=http%3A%2F%2Ftest.host%2Fusers")
     end
+    it "初期化時に指定した :redirect_url_format に従ってリダイレクト先が決定すること" do
+      get :index, :format => "hash", :opensocial_app_id => '12345'
+      response.should redirect_to("http://container.example.com/12345/?guid=ON&url=http%3A%2F%2Ftest.host%2Fusers%2Fnew")
+    end
   end
 
   context "コントローラで :redirect_url_format として :plain が指定されている場合" do
     controller do
       opensocial_wap :redirect_url_format => :plain
-      def index
-        redirect_to User.new
-      end
+      include TestController
     end
 
     it do
       get :index, :opensocial_app_id => '12345'
       response.should redirect_to("http://test.host/users")
     end
+    it do
+      get :index, :format => "hash", :opensocial_app_id => '12345'
+      response.should redirect_to("http://test.host/users/new")
+    end
   end
 
   context "コントローラで :redirect_url_format として :query が指定されている場合" do
     controller do
       opensocial_wap :redirect_url_format => :query
-      def index
-        redirect_to User.new
-      end
+      include TestController
     end
     it do
       get :index, :opensocial_app_id => '12345'
       response.should redirect_to("?guid=ON&url=http%3A%2F%2Ftest.host%2Fusers")
+    end
+    it do
+      get :index, :format => "hash", :opensocial_app_id => '12345'
+      response.should redirect_to("?guid=ON&url=http%3A%2F%2Ftest.host%2Fusers%2Fnew")
     end
   end
 
   context "コントローラで :redirect_url_format として :full が指定されている場合" do
     controller do
       opensocial_wap :redirect_url_format => :full
-      def index
-        redirect_to User.new
-      end
+      include TestController
     end
     it do
       get :index, :opensocial_app_id => '12345'
       response.should redirect_to("http://container.example.com/12345/?guid=ON&url=http%3A%2F%2Ftest.host%2Fusers")
+    end
+    it do
+      get :index, :format => "hash", :opensocial_app_id => '12345'
+      response.should redirect_to("http://container.example.com/12345/?guid=ON&url=http%3A%2F%2Ftest.host%2Fusers%2Fnew")
     end
   end
 end
