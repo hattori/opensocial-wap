f192c07d9be2714856a5484542e2d0576015207c
Tomonori Kusanagi <kusanagi@banana-systems.com>
2011-03-21 01:08:58 +0900

    POST parameter handling.

--- lib/opensocial-wap/oauth/request_proxy/basic_rack_request.rb
+++ lib/opensocial-wap/oauth/request_proxy/basic_rack_request.rb
@@ -4,6 +4,22 @@ module OpensocialWap
   module OAuth
     module RequestProxy
       class BasicRackRequest < ::OAuth::RequestProxy::RackRequest
+
+        private
+        
+        def request_params
+          request.POST
+          form_params = request.env['rack.request.form_vars']
+          if form_params && form_params.size > 0
+            form_params.split('&').inject({}) do |hsh, i| 
+              kv = i.split('=')
+              hsh[CGI::unescape(kv[0])] = kv[1] ? CGI::unescape(kv[1]) : ''
+              hsh 
+            end
+          else      
+            {}
+          end
+        end
       end
     end
   end
