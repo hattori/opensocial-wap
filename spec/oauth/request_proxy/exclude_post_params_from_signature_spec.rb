941d7c4a3b59ac19d36bbdaaec89046889b0ff7b
Tomonori Kusanagi <kusanagi@banana-systems.com>
2011-04-01 16:13:57 +0900

    Specs for the patch for request proxy.

---
+++ spec/oauth/request_proxy/exclude_post_params_from_signature_spec.rb
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+require 'opensocial-wap/oauth/request_proxy/exclude_post_params_from_signature'
+
+describe OpensocialWap::OAuth::RequestProxy::ExcludePostParamsFromSignature do
+
+  it "parameters_for_signatureにPOSTパラメータが含まれないこと" do
+    pending
+  end
+end
---
+++ spec/oauth/request_proxy/rack_request_patch_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+require 'opensocial-wap/oauth/request_proxy/rack_request_patch'
+
+describe OpensocialWap::OAuth::RequestProxy::RackRequestPatch do
+  
+  it "" do
+    pending
+  end
+end
