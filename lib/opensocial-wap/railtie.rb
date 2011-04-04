66b9191e4aa387e4fc2d9977e7cf898096c1d811
Tomonori Kusanagi <kusanagi@banana-systems.com>
2011-04-04 17:02:43 +0900

    Bug fix for non opensocial-wap environments.

--- lib/opensocial-wap/railtie.rb
+++ lib/opensocial-wap/railtie.rb
@@ -20,13 +20,15 @@ module OpensocialWap
     end
 
     initializer 'opensocial-wap.load_middleware', :after=> :load_config_initializers do
-      helper_class = config.opensocial_wap.oauth.helper_class
-
-      if helper_class
-        puts "opensocial-wap is enabled with #{helper_class}"
-        config.app_middleware.insert_before ActionDispatch::Cookies, OpensocialWap::Rack::OpensocialOauth, :helper_class => helper_class
-      else
-        puts "opensocial-wap is NOT enabled"
+      if config.opensocial_wap && config.opensocial_wap.oauth
+        helper_class = config.opensocial_wap.oauth.helper_class
+        
+        if helper_class
+          puts "opensocial-wap is enabled with #{helper_class}"
+          config.app_middleware.insert_before ActionDispatch::Cookies, OpensocialWap::Rack::OpensocialOauth, :helper_class => helper_class
+        else
+          puts "opensocial-wap is NOT enabled"
+        end
       end
     end
   end
