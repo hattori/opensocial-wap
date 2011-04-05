b669b13df2fe1acde87b50b3503ed03bc39bd6b2
Tomonori Kusanagi <kusanagi@banana-systems.com>
2011-04-05 23:04:02 +0900

    Spec for ClientHelper.

---
+++ spec/oauth/client_helper_spec.rb
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe OpensocialWap::OAuth::ClientHelper do
+
+  describe "#uri" do
+
+    it "構築したAPIエンドポイントが正しいこと" do
+      OpensocialWap::OAuth::Helpers::BasicHelper.setup(
+                                                       :consumer_key => 'abcde',
+                                                       :consumer_secret => 'fghijk',
+                                                       :api_endpoint => 'http://api.example.com/rest/'
+                                                       )
+      oauth_helper = OpensocialWap::OAuth::Helpers::BasicHelper.new
+      client_helper = OpensocialWap::OAuth::ClientHelper.new(oauth_helper, 
+                                                             'people', 
+                                                             '@me', 
+                                                             '@self', 
+                                                             :format => 'atom', :fields => 'gender,address')
+      client_helper.uri.to_s.should == "http://api.example.com/rest/people/@me/@self?fields=gender%2Caddress&format=atom"
+    end
+  end
+
+  describe "#authorization_header" do
+    pending
+  end
+end
